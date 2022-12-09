e1d08823f1e6d1901dd3407731affb47cd0d9a4f
Frank Charles Lugola <78613296+Lugofr01@users.noreply.github.com>
2022-12-09 17:05:54 -0600

    commit

--- src/projects/nameserver/nameserver.py
+++ src/projects/nameserver/nameserver.py
@@ -7,6 +7,7 @@
 """
 
 import argparse
+import struct
 import logging
 from socket import AF_INET, SOCK_DGRAM, socket
 
@@ -30,14 +31,21 @@ def val_to_n_bytes(value: int, n_bytes: int) -> tuple[int]:
     Split a value into n bytes
     Return the result as a tuple of n integers
     """
-    # TODO: Implement this function
+# TODO: Implement this function
     ...
+    i =tuple([value >> i*8 & 255 for i in range(n_bytes-1, -1, -1)])
+    return i
 
 
 def bytes_to_val(bytes_lst: list) -> int:
     """Merge n bytes into a value"""
-    # TODO: Implement this function
+# TODO: Implement this function
     ...
+    mergedValue = 0
+    byteLength =len(bytes_lst)
+    for j, i in enumerate(range(byteLength,0,-1)):
+        mergedValue += bytes_lst[i-1] << (8*j)
+    return mergedValue
 
 
 def get_left_n_bits(bytes_lst: list, n_bits: int) -> int:
@@ -45,8 +53,12 @@ def get_left_n_bits(bytes_lst: list, n_bits: int) -> int:
     Extract first (leftmost) n bits of a two-byte sequence
     Return the result as a decimal value
     """
-    # TODO: Implement this function
+# TODO: Implement this function
     ...
+    for i in bytes_lst:
+        result= bytes_to_val(bytes_lst) >> (16 - n_bits)
+    return result
+
 
 
 def get_right_n_bits(bytes_lst: list, n_bits: int) -> int:
@@ -56,6 +68,12 @@ def get_right_n_bits(bytes_lst: list, n_bits: int) -> int:
     """
     # TODO: Implement this function
     ...
+    for i in bytes_lst:
+        result =int(bin(bytes_to_val(bytes_lst))[18 - n_bits:20],2)
+    return result
+
+
+    
 
 
 def read_zone_file(filename: str) -> tuple:
@@ -63,18 +81,84 @@ def read_zone_file(filename: str) -> tuple:
     Read the zone file and build a dictionary
     Use domain names as keys and list(s) of records as values
     """
+# ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
     # TODO: Implement this function
     ...
+    dictionary = {}
+    values = ""
+    with open(filename) as file:
+        lines = file.readlines()
+
+        origin,time  = lines[0].split(" ")[1].replace("\n", "")[:-1],lines[1].split(" ")[1].replace("\n", "")
+
+      
+        for i in lines[2:-1]:
+            array = i.split(" ")
+            partions = [j for j in array if j != ""]
+        
+
+            if i[0] != " ":
+                values = partions[0]
+                part = partions[1:]
+            else:
+                part = partions
+            lengthPart = len(part) - 1
+
+            if lengthPart  >= 3:
+                time2Live = part[lengthPart - 3]
+            else:
+                time2Live = time
+
+                
+       
+            
+            if values not in dictionary:
+                dictionary[values] = [(time2Live, part[lengthPart - 2], part[lengthPart-1], part[lengthPart])]
+
+            else:    
+                dictionary[values].append((time2Live, part[lengthPart - 2], part[lengthPart - 1], part[lengthPart]))
+            
+                
+    return (origin, dictionary)
 
 
 def parse_request(origin: str, msg_req: bytes) -> tuple:
     """
-    Parse the request
-    Return query parameters as a tuple
+    Parse the request and return query parameters as a tuple.
     """
-    # TODO: Implement this function
-    ...
+    # Extract the domain name from the message and decode it.
+    domain_start = 12
+    domain_length = msg_req[12]
+    domain = msg_req[domain_start+1:domain_start+1+domain_length].decode()
 
+
+    
+    id = bytes_to_val(msg_req[0:2])
+
+
+
+
+
+
+    pointer = 0
+    while msg_req[domain_start:][pointer] > 0:
+        pointer += msg_req[domain_start:][pointer] + 1
+    
+
+    select= bytes_to_val(msg_req[domain_start:][pointer+1:pointer+3])
+    
+    if origin != "cs430.luther.edu":
+        raise ValueError("Unknown origin")
+    if select not in DNS_TYPES:
+        raise ValueError("Unknown query type")
+    if bytes_to_val(msg_req[domain_start:][pointer+3:pointer+5]) !=1:
+        raise ValueError("Unknown class")
+
+
+
+
+    return tuple([id,domain, select, msg_req[domain_start:]])
+ 
 
 def format_response(
     zone: dict, trans_id: int, qry_name: str, qry_type: int, qry: bytearray
@@ -82,6 +166,52 @@ def format_response(
     """Format the response"""
     # TODO: Implement this function
     ...
+
+    arrangement = DNS_TYPES[qry_type]
+
+
+    answers = [item for item in zone[qry_name] if item[2] == arrangement]
+    stored = len(answers)
+
+    response = struct.pack(">HHHHHH", trans_id, 0x8100, 1, stored, 0, 0)
+    response += qry
+    for i in answers:
+        response += bytes([0xC0, 0x0C])
+        response = response+ struct.pack(">HH", qry_type, 1)
+        timetoLive = TTL_SEC[i[0]]
+        cleanedTTLStr = val_to_n_bytes(timetoLive, 4)
+        time2Live = bytearray()
+        for j in cleanedTTLStr:
+            time2Live += bytes([j])
+        response += time2Live
+        stackAddress = i[len(i) - 1]
+
+
+        if qry_type == 1:
+            data_len = len(stackAddress.split("."))
+            response += struct.pack(">h", data_len)
+            addrArray = [bytes([int(item)]) for item in stackAddress.split(".")]
+            for i in addrArray:
+                response += i
+            
+
+        else:
+            response += struct.pack(">H", 16)
+            ans = stackAddress.split(":")
+
+            for i in ans:
+                ans = val_to_n_bytes(int(i, 16), 2)
+
+
+                for i in ans:
+                    response =response + bytes([i])
+            
+            
+
+    return response
+
+
+
 
 
 def run(filename: str) -> None:
@@ -103,8 +233,6 @@ def run(filename: str) -> None:
                 server_sckt.sendto(msg_resp, client_addr)
             except ValueError as v_err:
                 print(f"Ignoring the request: {v_err}")
-
-
 def main():
     """Main function"""
     arg_parser = argparse.ArgumentParser(description="Parse arguments")
